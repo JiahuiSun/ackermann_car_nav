e352aff389d6fb5c477d0ab64969d9255a63af90
joey <jhsun1997@sjtu.edu.cn>
2023-05-12 22:55:26 +0800

    process bag data

---
+++ src/mmwave_radar/script/nlos_sensing.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def line_by_2p(p1, p2):
+    """A ray from p1 to p2.
+    """
+    ABC = np.array([p2[1]-p1[1], p1[0]-p2[0], p2[0]*p1[1]-p1[0]*p2[1]])
+    if p1[1] > p2[1]:
+        ABC = -ABC
+    return ABC
+
+
+def nlosFilterAndMapping(pointCloud, radar_pos, corner_args):
+    """TODO: 现在只实现了L型转角
+    """
+    point_cloud_ext = np.concatenate([pointCloud[:, :2], np.ones((pointCloud.shape[0], 1))], axis=1)
+    top_wall_y = corner_args['top_wall_y']
+    side_wall_x = corner_args['side_wall_x']
+    inner_corner = corner_args['inner_corner']
+    # Filter
+    top_map_corner = [inner_corner[0], 2*top_wall_y - inner_corner[1]]
+    top_map_radar = [radar_pos[0], 2*top_wall_y-radar_pos[1]]
+    # 用前墙反射
+    top_border = top_map_corner[1]
+    # 如果转角在雷达左边
+    if inner_corner[0] < radar_pos[0]:
+        left_border = line_by_2p(radar_pos, inner_corner)
+        right_border = line_by_2p(top_map_radar, top_map_corner)
+    # 如果转角在雷达右边
+    elif inner_corner[0] > radar_pos[0]:
+        left_border = line_by_2p(top_map_radar, top_map_corner)
+        right_border = line_by_2p(radar_pos, inner_corner)
+    flag1 = point_cloud_ext.dot(left_border) > 0
+    flag2 = point_cloud_ext.dot(right_border) < 0
+    flag3 = pointCloud[:, 1] < top_border
+    flag = np.logical_and(np.logical_and(flag1, flag2), flag3)
+    # Mapping
+    pointCloud[flag, 1] = 2*top_wall_y - pointCloud[flag, 1]
+    point_cloud_filter = pointCloud[flag, :]
+    return point_cloud_filter
+
+
+def transform(radar_xy, delta_x, delta_y, yaw):
+    """Transform xy from radar coordinate to the world coordinate.
+    Inputs:
+        radar_xy: Nx2，雷达坐标系下的点云
+        delta_x, delta_y: 雷达在世界坐标系中的坐标
+        yaw: 雷达坐标系逆时针旋转yaw度与世界坐标系重合
+    Return:
+        world_xy: Nx2
+    """
+    yaw = yaw * np.pi / 180
+    rotation_matrix = np.array([[np.cos(yaw), np.sin(yaw)], [-np.sin(yaw), np.cos(yaw)]])
+    translation_vector = np.array([delta_x, delta_y]).reshape(-1, 1)
+    world_xy = (rotation_matrix.dot(radar_xy.T) + translation_vector).T
+    return world_xy
---
+++ src/mmwave_radar/script/plan1_for_bag.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from nlos_proc import transform
+import rosbag
+from sensor_msgs import point_cloud2
+
+
+fig, ax = plt.subplots(figsize=(10, 4))
+line0, = ax.plot([], [], 'ob', ms=2)
+line1, = ax.plot([], [], 'or', ms=2)
+lines = [line0, line1]
+
+
+def init_fig():
+    ax.set_xlabel('x(m)')
+    ax.set_ylabel('y(m)')
+    ax.set_xlim([-5, 5])
+    ax.set_ylim([-5, 10])
+    ax.plot([-5, 5], [0, 0], 'k')
+    return lines
+
+def gen_data():
+    for topic, msg, t in rosbag.Bag(
+        "/home/dingrong/Code/ackermann_car_nav/test_2023-05-11-19-45-10.bag", 'r'):
+        if topic == '/mmwave_radar_point_cloud':
+            points = point_cloud2.read_points_list(
+                msg, field_names=['x', 'y', 'z', 'vel']
+            )
+            x_pos = [p.x for p in points]
+            y_pos = [p.y for p in points]
+            z_pos = [p.z for p in points]
+            vel = [p.vel for p in points]
+            point_cloud = np.array([x_pos, y_pos, z_pos, vel]).T
+            point_cloud[:, :2] = transform(point_cloud[:, :2], -0.043, 0.13, 330)
+            yield point_cloud, msg.header.seq
+
+def visualize(result):
+    adc_data, seq = result
+    x_pos, y_pos, dopplers = adc_data[:, 0], adc_data[:, 1], adc_data[:, -1]
+    static_idx = dopplers == 0
+    dynamic_idx = dopplers != 0
+    ax.set_title(f"frame id: {seq}")
+    lines[0].set_data(x_pos[static_idx], y_pos[static_idx])
+    lines[1].set_data(x_pos[dynamic_idx], y_pos[dynamic_idx])
+
+
+ani = animation.FuncAnimation(
+    fig, visualize, gen_data, interval=100,
+    init_func=init_fig, repeat=False, save_count=100
+)
+ani.save("plan1-2.gif", writer='imagemagick')
+plt.show()
